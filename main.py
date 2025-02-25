cfb1a261ebdac0c95ab54ae8331110b55515f26a
MatheusW166 <matheuswagnerdossantos@outlook.com>
2025-02-24 22:54:16 -0400

    feat: function that fills content.xml tables by name

--- main.py
+++ main.py
@@ -1,82 +1,60 @@
 import zipfile
 import re
-import shutil
-import os
 
-# Arquivos de entrada e saída
-orig_file = "ATA_CORREICAO_WEB.docx.odt"
-copy_file = "ATA_CORREICAO_WEB_COPIA.odt"
+orig_file = "template.odt"
+generated_file = "_generated_doc.odt"
 temp_content = "content_temp.xml"
 
-# Criar uma cópia do ODT original
-shutil.copy(orig_file, copy_file)
 
-# Extrair o content.xml
-with zipfile.ZipFile(copy_file, "r") as files:
-    content = files.read("content.xml").decode()
+def fill_table(name: str, content_xml: str, data=[]):
+    table_match = list(re.finditer(rf"<table:table.*?{name}.*?</table:table>", content_xml))[0]
+    row_match = list(re.finditer(r"<table:table-row.*?</table:table-row>", table_match.group()))
+
+    row_match_data = row_match[-2]
+
+    row_template = row_match_data.group()
+    rows = ""
+
+    columns = data[0]
+    for d in data[1:]:
+        row = row_template
+        for i, v in enumerate(d):
+            var = "{{" + f" d.{columns[i]} " + "}}"
+            row = row.replace(var, v)
+        rows += row
+
+    ini, end = row_match[1].span()[0], row_match[-1].span()[1]
+    table = table_match.group()[:ini] + rows + table_match.group()[end:]
+
+    ini, end = table_match.span()
+    return content_xml[:ini] + table + content_xml[end:]
+
+
+def create_no_content_copy(orig_file: str, generated_file: str):
+    with zipfile.ZipFile(orig_file, "r") as zip_read:
+        with zipfile.ZipFile(generated_file, "w", zipfile.ZIP_DEFLATED) as zip_write:
+            for item in zip_read.infolist():
+                if item.filename != "content.xml":  # Ignorar o content.xml antigo
+                    zip_write.writestr(
+                        item.filename, zip_read.read(item.filename))
+
+
+create_no_content_copy(orig_file, generated_file)
 
 # Dados a serem inseridos
 data = [
-    {
-        "inicio": "2024-01-01",
-        "fim": "2024-12-31",
-        "egestao_inicio": "2025-12-24",
-        "egestao_fim": "2026-05-01",
-        "vara": "1ª Vara Federal de São Paulo",
-    },
-    {
-        "inicio": "2027-05-15",
-        "fim": "2028-10-30",
-        "egestao_inicio": "2021-01-01",
-        "egestao_fim": "2022-12-31",
-        "vara": "2ª Vara Federal de São Paulo",
-    }
+    ["inicio", "fim", "egestao_inicio", "egestao_fim", "vara"],
+    ["2024-01-01", "2024-12-31", "2025-12-24", "2026-05-01", "1ª Vara Federal de São Paulo"],
+    ["2027-05-15", "2028-10-30", "2021-01-01", "2022-12-31", "2ª Vara Federal de São Paulo"]
 ]
 
-# Encontrar a tabela
-t1_match = list(re.finditer(
-    r"<table:table.*?DADOS_CORREICAO.*?</table:table>", content))[0]
-row_match = list(re.finditer(
-    r"(<table:table-row.*?</table:table-row>)", t1_match.group()))[-2]
+with zipfile.ZipFile(orig_file, "r") as files:
+    content = files.read("content.xml").decode()
 
-# Criar as novas linhas
-row_template = row_match.group()
-rows = ""
-for d in data:
-    row = row_template
-    for k, v in d.items():
-        var = "{{" + f" d.{k} " + "}}"
-        row = row.replace(var, v)
-    rows += row
-
-# Inserir as novas linhas no XML
-ini, end = row_match.span()
-t1 = t1_match.group()[:ini] + rows + t1_match.group()[end:]
-
-ini, end = t1_match.span()
-content = content[:ini] + t1 + content[end:]
-
-# Salvar o novo content.xml temporariamente
-with open(temp_content, "w", encoding="utf-8") as temp_file:
-    temp_file.write(content)
-
-# Criar um novo ODT sem o antigo content.xml
-with zipfile.ZipFile(copy_file, "r") as zip_read:
-    with zipfile.ZipFile("temp.odt", "w", zipfile.ZIP_DEFLATED) as zip_write:
-        for item in zip_read.infolist():
-            if item.filename != "content.xml":  # Ignorar o content.xml antigo
-                zip_write.writestr(item.filename, zip_read.read(item.filename))
-
-
-# Substituir o arquivo original pelo modificado
-os.replace("temp.odt", copy_file)
+content = fill_table("DADOS_CORREICAO", content, data)
 
 # Adicionar o novo content.xml ao ODT
-with zipfile.ZipFile(copy_file, "a") as zip_mod:
-    with open(temp_content, "rb") as temp_file:
-        zip_mod.writestr("content.xml", temp_file.read())
+with zipfile.ZipFile(generated_file, "a") as zip_mod:
+    zip_mod.writestr("content.xml", content)
 
-# Remover o arquivo temporário
-os.remove(temp_content)
-
-print(f"Arquivo modificado salvo como: {copy_file}")
+print(f"Arquivo modificado salvo como: {generated_file}")
