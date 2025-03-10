7a59b5dfbf23cbe327fcdf6e7b2f19c935f5e26a
MatheusW166 <matheuswagnerdossantos@outlook.com>
2025-03-10 12:49:48 -0400

    ref: changing directories

--- src/app/spi/db_dynamic_interface.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class DbDynamicConnectionInterface(ABC):
-    @abstractmethod
-    def execute(self, query: str, conn_string: str):
-        raise NotImplementedError()
--- src/domain/generic_query_result_model.py
+++
@@ -1,9 +0,0 @@
-from src.domain.base_model import BaseModel
-from typing import Any, Iterable
-from dataclasses import dataclass
-
-
-@dataclass
-class GenericQueryResultModel(BaseModel):
-    name: str
-    result: dict[str, Iterable[Any]]
