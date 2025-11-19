9ee791e7bd3e8c598a8d403d499c8f3b748cf149
MatheusW166 <matheuswagnerdossantos@outlook.com>
2025-11-19 02:01:09 -0400

    wip: working on on-demand rendering

--- main.py
+++ main.py
@@ -1,138 +1,139 @@
-# import os
-# import asyncio
-
-# from src.adapters.spi.gd_consulta.db_connection import DbConnection
-# from src.adapters.spi.query_executor.db_dynamic_connection import DbDynamicConnection
-# from src.adapters.repositories.gd_consulta.gd_consulta_repository import GdConsultaRepository
-# from src.adapters.repositories.query_executor.query_executor_repository import QueryExecutorRepository
-
-# from src.infra.configuration.config_mapper import ConfigurationMapper
-# from src.infra.core.odt_manipulator import OdtManipulator
-# from src.infra.core.content_xml_processor import ContentXMLProcessor
-# from src.infra.shared.logs import Logger
-
-# from src.domain.configuration_model import ConfigurationModel
-# from src.domain.gd_consulta_model import GDConsultaModel
-
-# env = os.getenv("ENV", "dev")
-# config: ConfigurationModel = ConfigurationMapper.get_config(env)
-
-# main_db_connection = DbConnection(config)
-# gd_consulta_repository = GdConsultaRepository(main_db_connection)
-
-# odt = OdtManipulator(orig_file="template.odt", generated_file="_generated.odt")
-# xml = odt.load_contentxml()
-
-# processor = ContentXMLProcessor(xml)
-# tables = processor.find_all_vars()
-
-# consultas = gd_consulta_repository.get_all_in_names(tables)
-
-# dynamic_db_connection = DbDynamicConnection(config.pool_size)
-# query_executor_repository = QueryExecutorRepository(dynamic_db_connection)
-
-# log = Logger.get_logger()
-
-
-# async def exec_query_async(query: GDConsultaModel):
-#     loop = asyncio.get_running_loop()
-#     try:
-#         # Executing each operation in a separate thread
-#         result = await loop.run_in_executor(
-#             None,
-#             query_executor_repository.execute,
-#             query.name,
-#             query.query,
-#             config.postgres_url
-#         )
-
-#         # Locking to ensure that only one thread can modify the xml at a time
-#         async with asyncio.Lock():
-#             processor.fill_content(result.name, result.result)
-#     except Exception as e:
-#         log.error(f"Error on {query.name}: {e}")
-#         pass
-
-
-# async def main():
-#     await asyncio.gather(*[exec_query_async(c) for c in consultas])
-#     odt.generate_document(processor.tostring())
-
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
-
-
+from jinja2 import Undefined, Environment, meta
 import re
+from typing import cast
 from docxtpl import DocxTemplate
 
-# First render
-doc = DocxTemplate("template.docx")
-doc.render({
-    "TABLE": [
-        {"a": 1, "b": 4},
-        {"c": 3, "d": 7}
-    ],
-})
+# # First render
+# doc = DocxTemplate("template.docx")
+# doc.render({
+#     "LISTA": ["A","B","C"],
+# })
 
-# Second render
-doc.render({
-    "ANOTHER_TABLE": [
-        {"a": 5, "b": 7},
-        {"c": 5, "d": 5}
-    ],
-})
+# # Second render
+# doc.render({
+#      "LISTA2": ["A","B","C"],
+# })
 
-doc.save("_generated.docx")
+# doc.save("_generated.docx")
 
-exit(0)
+# exit(0)
 
 
-clob = open("doc.xml", "r").read()
+def match(pattern: str, text: str, group=0):
+    m = re.match(pattern, text, flags=re.DOTALL)
+
+    if m:
+        try:
+            return cast(str, m.group(group))
+        except:
+            pass
+
+    return None
 
 
-def extract_jinja_blocks(xml_content: str):
+def extract_complete_structures(xml: str):
+    tokens: list[str] = re.split(r"(\{\%.*?\%\})", xml, flags=re.DOTALL)
 
-    # Padrões para detectar início e fim de blocos
-    open_pattern = re.compile(r"\{\%\s*(for|if|block)\b.*?\%\}")
-    close_pattern = re.compile(r"\{\%\s*end(for|if|block)\s*\%\}")
+    # Anything like {%...%} without "end"
+    open_pattern = r"\{\%\s*(?!.*end).*?\%\}"
 
-    # Tokenizar o texto em partes Jinja e não-Jinja
-    tokens = re.split(r"(\{\%.*?\%\})", xml_content, flags=re.DOTALL)
+    # Anything like {% end... %}
+    close_pattern = r"\{\%\s*end.*?\%\}"
 
-    structures = []
-    stack = []
-    current_block = ""
+    # Gets the reserved word, example:
+    # {% for... %} => for, {% if... %} => if
+    reserved_word_pattern = r"\{\%\s*(\S+)"
+
+    close_block_stack: list[str] = []
+    structures: list[str] = []
+    structure = ""
 
     for token in tokens:
-        if not token.strip():
+        open_block = match(open_pattern, token)
+
+        if open_block:
+            structure += open_block
+            reserved_word = match(reserved_word_pattern, open_block, 1)
+
+            if not reserved_word:
+                raise RuntimeError(open_block)
+
+            end_block = "end"+reserved_word
+            close_block_stack.append(end_block)
             continue
 
-        if open_pattern.match(token):
-            # início de um bloco
-            if not stack:
-                current_block = token  # inicia novo bloco
-            else:
-                current_block += token
-            stack.append(token)
+        close_block = match(close_pattern, token)
+        if not close_block:
+            structure += token
+            continue
 
-        elif close_pattern.match(token):
-            # fim de um bloco
-            current_block += token
-            if stack:
-                stack.pop()
-            if not stack:
-                # bloco completo finalizado
-                structures.append(current_block.strip())
-                current_block = ""
-        else:
-            # conteúdo XML ou texto entre os blocos
-            if stack:
-                current_block += token
+        if not close_block_stack:
+            raise RuntimeError("No open block found\n"+structure)
+
+        if close_block_stack[-1] in close_block:
+            structure += close_block
+            close_block_stack.pop()
+
+        if not close_block_stack:
+            structures.append(structure.strip())
+            structure = ""
+    else:
+        structures[-1] += tokens[-1]
 
     return structures
 
 
-result = extract_jinja_blocks(clob)
+"""
+TODO:
+Problema => dada uma variável, quais as outras que devo consultar para
+renderizar todos os blocos em que ela aparece? E quais os indíces desses blocos?
+"""
 
-print(result[1])
+
+def index_vars_in_structures(structures: list[str]):
+
+    from time import time
+    s = time()
+
+    env = Environment()
+
+    def get_vars(xml: str):
+        parsed = env.parse(xml)
+        return meta.find_undeclared_variables(parsed)
+
+    for idx, structure in enumerate(structures):
+        vars = get_vars(structure)
+        print(vars)
+
+    print(time() - s)
+
+
+class KeepVarUndefined(Undefined):
+    """
+    Keeps {{ ... }} untouched if it's not found in the context dict
+    """
+
+    def __str__(self):
+        return f"{{{{ {self._undefined_name} }}}}"
+
+    def __getattr__(self, name):
+        return f"{{{{ {self._undefined_name}.{name} }}}}"
+
+    def __getitem__(self, key):
+        return f"{{{{ {self._undefined_name}['{key}'] }}}}"
+
+
+with open("pre-processed.xml", "r") as f:
+    clob = f.read()
+
+structures = extract_complete_structures(clob)
+
+index_vars_in_structures(structures)
+
+exit(0)
+
+env = Environment(trim_blocks=True, undefined=KeepVarUndefined)
+
+rendered = env.from_string(result[0]).render(
+    {"LISTA": ["A", "B", "C"], "VAR_NAO_DEFINIDA": 1})
+print(rendered)
