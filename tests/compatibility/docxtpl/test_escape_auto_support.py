ef4e1502c02836f8794784910f1de9f8339c62ad
Matheus Wagner <matheuswagnerdossantos@outlook.com>
2026-01-05 16:45:14 -0400

    ref: using features for cleaner code

--- tests/compatibility/docxtpl/test_escape_auto_support.py
+++ tests/compatibility/docxtpl/test_escape_auto_support.py
@@ -4,6 +4,8 @@ from tests.helpers import paths as p, docx
 from src.templaterx import TemplaterX
 import pytest
 
+XML_RESERVED = r"""<"&'>"""
+
 
 @pytest.fixture
 def paths(tmp_path):
@@ -13,18 +15,39 @@ def paths(tmp_path):
     )
 
 
-def test_escape_auto_support(paths):
-    # NOTE: All renders must be done before saving.
+@pytest.fixture
+def context():
+    return {
+        "nested_dict": get_nested_dict(),
+        "autoescape_unicode": "This is an escaped <unicode> example \u4f60 & \u6211",
+        "iteritems": iteritems,
+    }
 
-    XML_RESERVED = r"""<"&'>"""
+
+# -------------------------
+# Helpers
+# -------------------------
+
+
+def get_nested_dict():
+    return {name(str(c)): c for c in XML_RESERVED}
+
+
+def iteritems(x: dict):
+    return x.items()
+
+
+# -------------------------
+# Tests
+# -------------------------
+
+
+def test_escape_auto_support(paths, context):
+    # NOTE: All renders must be done before saving.
     tplx = TemplaterX(paths.template, autoescape=True)
 
     tplx.render({})
-    tplx.render({
-        "nested_dict": {name(str(c)): c for c in XML_RESERVED},
-        "autoescape_unicode": "This is an escaped <unicode> example \u4f60 & \u6211",
-        "iteritems": lambda x: x.items(),
-    })
+    tplx.render(context)
     tplx.render({"autoescape": 'Escaped "str & ing"!'})
 
     xml = docx.get_rendered_xml(tplx, paths.out)
@@ -38,16 +61,10 @@ def test_escape_auto_support(paths):
     assert "This is an escaped &lt;unicode&gt; example 你 &amp; 我" in xml
 
 
-def test_undefined_context_should_have_its_placeholders_preserved(paths):
-
-    XML_RESERVED = r"""<"&'>"""
+def test_undefined_context_should_have_its_placeholders_preserved(paths, context):
     tplx = TemplaterX(paths.template, autoescape=True)
 
-    tplx.render({
-        "nested_dict": {name(str(c)): c for c in XML_RESERVED},
-        "autoescape_unicode": "This is an escaped <unicode> example \u4f60 & \u6211",
-        "iteritems": lambda x: x.items(),
-    })
+    tplx.render(context)
 
     xml = docx.get_rendered_xml(tplx, paths.out)
 
